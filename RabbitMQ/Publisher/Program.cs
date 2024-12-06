e9a6bf464b88a41c0d4301bd8ff668db42dd5e24
memo-can <mehmetcan-b@hotmail.com>
2024-12-06 13:00:54 +0300

    first commit

---
+++ RabbitMQ/Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+         var configuration = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory) 
+            .AddJsonFile("Settings.json", optional: true, reloadOnChange: true) 
+            .Build();
+
+        var factory = new ConnectionFactory();
+
+        factory.Uri = new Uri(configuration["AppSettings:Connection"]);
+
+        using var connection = await factory.CreateConnectionAsync();
+        var channel = await connection.CreateChannelAsync();
+        await channel.QueueDeclareAsync("queue-test", true, false, false);
+
+        var messageBody = Encoding.UTF8.GetBytes("Test Message sent by publisher");
+
+        await channel.BasicPublishAsync(string.Empty, "queue-test",false, messageBody);
+
+        Console.WriteLine("The message send");
+        Console.ReadLine();
+
+    }
+}
---
+++ RabbitMQ/Subscriber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+                    .SetBasePath(AppContext.BaseDirectory) 
+                    .AddJsonFile("Settings.json", optional: true, reloadOnChange: true) 
+                    .Build();
+
+
+        var factory = new ConnectionFactory();
+
+        factory.Uri = new Uri(configuration["AppSettings:Connection"]);
+
+        using var connection = await factory.CreateConnectionAsync();
+        using var channel = await connection.CreateChannelAsync();
+
+        Console.WriteLine(" [*] Waiting for messages.");
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        consumer.ReceivedAsync += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($" [x] Received {message}");
+            return Task.CompletedTask;
+        };
+
+        await channel.BasicConsumeAsync("queue-test", autoAck: true, consumer: consumer);
+
+        Console.ReadLine();
+
+    }
+}
